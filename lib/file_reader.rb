de877b04d5d753e4f0fa4f806efea9f6d979ef3f
Remco van Stiphout <vanstiphout@hotmail.com>
2009-03-10 11:19:58 -0400

    Broken mess with context and error_list added

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -2,51 +2,11 @@ require 'shovel_file'
 require 'shovel_directory'
 
 class FileReader
-  def initialize start_directory = nil, started = true
+  def initialize actions = [], start_directory = nil, started = true
     @start_directory = start_directory
     @started = started
-  end
-
-  def create_shovel_file full_path
-    if !File.exists?(full_path)
-      puts "\nFile with full_path #{full_path} does not exist."
-      return nil
-    end
-    shovel_file = ShovelFile.new
-    shovel_file.original_full_path = full_path
-    shovel_file.original_path = File.split(full_path)[0]
-    shovel_file.original_file_name = File.split(full_path)[1]
-    shovel_file.file_size = File.size(full_path)
-    shovel_file.icpn = find_icpn full_path
-    shovel_file.md5_checksum = Digest::MD5.hexdigest(File.read(full_path))
-    return shovel_file
-  end
-
-  def create_shovel_directory full_path
-    shovel_directory = ShovelDirectory.new
-    shovel_directory.original_full_path = full_path
-    file_count = 0
-    Dir.glob('*').each do |element|
-      if !File.directory? element
-        file_count = file_count + 1
-      end
-    end
-    shovel_directory.file_count = file_count
-    shovel_directory.icpn = find_icpn full_path
-    return shovel_directory
-  end
-
-  def find_icpn full_path
-    if File.directory? full_path
-      directory = full_path.split('/')[-1]
-    else
-      directory = full_path.split('/')[-2]
-    end
-
-    if directory.length == 13 && directory =~ /^[0-9]*$/
-      return directory
-    end
-    return nil
+    @actions = actions
+    @error_list = []
   end
 
   def process_directory full_path
@@ -56,45 +16,27 @@ class FileReader
       puts "we are starting!"
       @started = true
     end
-    shovel_directory = find_shovel_directory full_path
-    if !shovel_directory
-      shovel_directory = create_shovel_directory full_path
-      shovel_directory.save
-    end
 
     directories = []
     Dir.glob('*').each do |element|
       if File.directory? element
         directories << element
       elsif @started
-        process_file full_path + '/' + element
+        # execute each of the actions against the file
+        context = {:file => true}
+        @actions.each do |action|
+          @error_list.concat(action.execute(full_path + '/' + element, context))
+        end
       end
     end
     directories.each do |directory|
-      process_directory full_path + '/' + directory
+      # execute each of the actions against the directory
+      @actions.each do |action|
+        context = {:directory => true}
+        @error_list.concat(action.execute(full_path + '/' + directory, context))
+      end
+      @error_list.concat(process_directory(full_path + '/' + directory))
     end
-  end
-
-  def process_file full_path
-    # is there already a record for this in Couch?
-    shovel_file = find_shovel_file full_path
-    if !shovel_file
-      shovel_file = create_shovel_file full_path
-      shovel_file.save
-    else
-      puts "Ignoring file as it already exists in Couch."
-    end
-  end
-
-   def find_shovel_directory full_path
-    shovel_directories = ShovelDirectory.find_by_original_full_path :all, full_path
-    return shovel_directories[0] if shovel_directories.length == 1
-    return nil
-  end
-
-  def find_shovel_file full_path
-    shovel_files = ShovelFile.find_by_original_full_path :all, full_path
-    return shovel_files[0] if shovel_files.length == 1
-    return nil
+    return []
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/init')
 require 'file_reader'
+require 'jobs/write_to_couch'
+require 'jobs/write_to_s3'
 require 'optparse'
 
 puts "Creating database if necessary"
@@ -7,6 +9,10 @@ Lazyboy::Setup.create_databases
 Lazyboy::Setup.create_indexes
 
 puts "Starting FileReader"
+
+write_to_couch_strategy = WriteToCouch.new
+write_to_s3_strategy = WriteToS3.new
+actions = [write_to_couch_strategy, write_to_s3_strategy]
 
 # command-line arguments:
 #  -t top-level directory
@@ -18,5 +24,6 @@ opts.on('-t', '--top_directory VAL', String)    { |val| top_directory = val }
 opts.on('-d', '--start_directory VAL', String)    { |val| start_directory = val }
 opts.parse!(ARGV)
 puts "starting with top_directory = '#{top_directory}' and start_directory = '#{start_directory}'"
-file_reader = FileReader.new start_directory, start_directory == nil
-file_reader.process_directory top_directory
+file_reader = FileReader.new actions, start_directory, start_directory == nil
+error_list = file_reader.process_directory top_directory
+puts "Done, error_list: #{error_list.inspect}"
